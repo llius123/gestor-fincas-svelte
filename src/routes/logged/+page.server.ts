be4da666a70d077846a3a073d342b3901aa44870
Yisus <llius619@gmail.com>
2024-02-20 20:57:08 +0100

    feat: logged route now is protected

--- src/routes/logged/+page.server.ts
+++ src/routes/logged/+page.server.ts
@@ -1,10 +1,12 @@
 import { getTokenCookie } from '$lib/front/tokenService';
 import { verifyAccessToken } from '$lib/server/db/login/token-service';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({cookies}) => {
-    const token = getTokenCookie({cookies})
-	return {
-		valid: await verifyAccessToken(token || '')
-	};
+export const load: PageServerLoad = async ({ cookies }) => {
+	const token = getTokenCookie({ cookies });
+	const valid = await verifyAccessToken(token || '');
+	if (!valid) {
+		redirect(301, '/');
+	}
 };
